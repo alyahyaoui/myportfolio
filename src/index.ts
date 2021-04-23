a85991599e375ad8f1374f46f1e5a065cc456289
alyahyaoui <ali.codeur@gmail.com>
2021-04-23 04:37:16 +0100

    splitting project into modules

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,8 @@
-class Header {
-	render() {
-		const header = document.createElement('header');
-		const menuBtn = document.createElement('button');
-		const logo = document.createElement('a');
-		const search = document.createElement('input');
-
-		header.classList.add('header');
-		menuBtn.classList.add('menuBtn');
-		logo.classList.add('logo');
-		search.classList.add('search');
-		header.append(menuBtn, logo, search);
-		return header;
-	}
-}
-class Drawer {
-	render() {
-		const drawer = document.createElement('div');
-		const navMenu = document.createElement('ul');
-		drawer.classList.add('drawer');
-		navMenu.classList.add('navMenu');
-		['todo', 'link2', 'link3'].forEach((link) => {
-			navMenu.appendChild(new NavItem(link).render());
-		});
-		drawer.appendChild(navMenu);
-		return drawer;
-	}
-}
-
-class NavItem {
-	content;
-	constructor(content: string) {
-		this.content = content;
-	}
-	render() {
-		const navItem = document.createElement('li');
-		const navLink = document.createElement('a');
-		navItem.classList.add('navItem');
-		navLink.classList.add('navLink');
-		navLink.textContent = this.content;
-		navItem.appendChild(navLink);
-		return navItem;
-	}
-}
-
-class Main {
-	render() {
-		const main = document.createElement('main');
-		main.classList.add('main');
-		return main;
-	}
-}
-
-class Footer {
-	render() {
-		const footer = document.createElement('footer');
-		footer.classList.add('footer');
-		footer.innerHTML = '<h6>Powered by Ali Yahyaoui</h6>';
-		return footer;
-	}
-}
+//? getters and setters make you able to access properties and modify em from outside of the class
+import Header from './modules/header.js';
+import Drawer from './modules/drawer.js';
+import Main from './modules/main.js';
+import Footer from './modules/footer.js';
 
 class App {
 	static render() {
@@ -66,9 +10,9 @@ class App {
 
 		root.append(
 			new Header().render(),
-			new Drawer().render(),
+			new Drawer(['todo', 'habit', 'exprense']).render(),
 			new Main().render(),
-			new Footer().render()
+			new Footer().render(),
 		);
 	}
 }
